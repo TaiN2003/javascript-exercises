036692998955543c3c4b33c3a68c4175d4872cc1
Tai Nguyen <tai.ah.nguyen2003@gmail.com>
2026-08-12 02:10:55 -0400

    07_reverseString all tests passed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split('');
+    
+    let leftPointer = 0;
+    let rightPointer = string.length - 1;
 
+    let char = ''
+
+    while (leftPointer < rightPointer) {
+        char = arr[leftPointer];
+        arr[leftPointer] = arr[rightPointer];
+        arr[rightPointer] = char;
+        
+        leftPointer++;
+        rightPointer--;
+    }
+
+    const result = arr.join('');
+
+    return result;
 };
 
 // Do not edit below this line
