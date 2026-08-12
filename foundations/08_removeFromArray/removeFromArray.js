d5d22a62e629cdd2f666d0b608689a2d0c16001b
Tai Nguyen <tai.ah.nguyen2003@gmail.com>
2026-08-12 02:29:02 -0400

    08_removeFromArray first tests passed

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    const filteredArr = arr.filter(arrNum => arrNum !== num);
+    
+    return filteredArr;
 };
 
 // Do not edit below this line
