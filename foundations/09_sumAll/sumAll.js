bdaa7fdf1fd3913bef889c4991d3a1e093bf1e6b
Tai Nguyen <tai.ah.nguyen2003@gmail.com>
2026-08-13 23:37:12 -0400

    09_sumAll all tests passed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 > 0 && num1 > 0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+        smallerNum = 0;
+        biggerNum = 0;
+    
+        if (num1 < num2) {
+            smallerNum = num1;
+            biggerNum = num2;
+        }
+        else {
+            smallerNum = num2;
+            biggerNum = num1;
+        }
 
+        let sum = 0;
+
+        for (let i = smallerNum; i < biggerNum + 1; i++) {
+            sum = sum + i;
+        }
+
+        return sum;
+    }
+    
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
