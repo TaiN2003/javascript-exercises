19e93a9fb4a608ce130cbaa55c126699212f2418
Tai Nguyen <tai.ah.nguyen2003@gmail.com>
2026-08-17 23:29:58 -0400

    11_tempConversion all tests passed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let fahToCel = Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
+  return fahToCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let celToFah = Math.round(((celsius * 9/5) + 32) * 10) / 10;
+  return celToFah;
 };
 
 // Do not edit below this line
